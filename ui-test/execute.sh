779db5deed11331246c2f0c9301892430a79a748
krisnababu <krisnababu@gmail.com>
2021-12-22 01:39:57 +0530

    Update execute.sh

--- ui-test/execute.sh
+++ ui-test/execute.sh
@@ -1,11 +1,13 @@
 #/bin/bash!
-	source venv/bin/activate
-	pip3 install -r requirements.txt
-	      /usr/bin/rm -f reports/*
-	      ~/.local/bin/behave -f allure_behave.formatter:AllureFormatter -o reports/ features/
-	      ~/allure/bin/allure serve reports/ > stdout 2>&1 &
-	      PID=$!
-	      /usr/bin/kill -9 $PID
-	      line=`/usr/bin/grep 'tmp' stdout`
-	      id=`/usr/bin/echo $line | /usr/bin/cut -d '/' -f 3`
-	      /usr/bin/cp -rf /tmp/$id/allure-report/* /var/www/html/testreport/
+source venv/bin/activate
+pip3 install -r requirements.txt
+/usr/bin/rm -f reports/*
+~/.local/bin/behave -f allure_behave.formatter:AllureFormatter -o reports/ features/
+~/allure/bin/allure serve reports/ > id 2>&1 &
+PID=$!
+sleep 5
+if [ -s id ]; then
+        /usr/bin/cp -rf /tmp/`/usr/bin/grep 'tmp' id | /usr/bin/cut -d '/' -f 3`/allure-report/* /var/lib/jenkins/workspace/testreport/
+fi
+/usr/bin/kill -9 $PID
+/usr/bin/rm id
