cecfa7484c7ca8923b25652d8c05ce9fe3e66ae8
krisnababu <krisnababu@gmail.com>
2021-12-22 01:47:02 +0530

    Update transtats-login-steps.py

--- ui-test/features/steps/transtats-login-steps.py
+++ ui-test/features/steps/transtats-login-steps.py
@@ -13,8 +13,8 @@ def launch(context):
 
 @when('Open transtats homepage')
 def ts_home(context):
-    context.driver.get("http://10.65.207.9:8014/")
-    time.sleep(5)
+    context.driver.get("http://10.65.207.9")
+    
 @when('Click on login')
 def get_login(context):
     context.driver.find_element_by_xpath("/html/body/nav/nav/ul/li[3]/a").click()
@@ -28,18 +28,17 @@ def step_impl(context, usr, passwd):
 @when('Click on login button')
 def click_login(context):
     context.driver.find_element_by_xpath("/html/body/div/div[2]/div/form/div[3]/input").click()
-    time.sleep(2)
 
 @then('Successful login to the Transtats admin page.')
 def admin_page(context):
     try:
         txt = context.driver.find_element_by_xpath('//*[@id="content"]/h1').text
     except:
-        assert True, "login failed"
+        assert False, "login failed"
     else:
         if txt == "Site administration":
             assert True, "login success"
         else:
-            assert True, "login failed"
+            assert False, "login failed"
     finally:
         context.driver.close()
